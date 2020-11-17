96a24d5056b56f78036cd3107cf1a60e3f725a3d
Francesco Bertolaccini <francesco@bertolaccini.dev>
2020-11-17 11:56:46 +0100

    Edit API responses

--- app.py
+++ app.py
@@ -24,7 +24,28 @@ def new_notification(body):
     """ Sends a new notification to a user """
     noti = Notification()
     noti.user_id = body['user_id']
-    noti.sent_on = parser.isoparse(body['sent_on'])
+
+    try:
+        sent_on = parser.isoparse(body['sent_on'])
+
+        if sent_on > datetime.datetime.now():
+            return {"error": "Invalid sent_on date"}, 400
+
+        noti.sent_on = parser.isoparse(body['sent_on'])
+    except ValueError:
+        return {"error": "Invalid sent_on date"}, 400
+    
+    if 'read_on' in body:
+        try:
+            read_on = parser.isoparse(body['read_on'])
+
+            if read_on > datetime.datetime.now() or read_on < noti.sent_on:
+                return {"error": "Invalid read_on date"}, 400
+
+            noti.read_on = read_on
+        except ValueError:
+            return {"error": "Invalid read_on date"}, 400
+
     noti.content = body['content']
     
     db.session.add(noti)
@@ -59,18 +80,21 @@ def edit_notification(notification_id, body):
     if q is None:
         return None, 404
     else:
-        if 'read_on' in body:
+        if q.read_on is not None:
+            return {"error": "Cannot modify already present read_on date"}, 400
+
+        try:
             read_on = parser.isoparse(body['read_on'])
+        except ValueError:
+            return {"error": "Invalid read_on date"}, 400
 
-            if read_on < q.sent_on:
-                return {"error": "Invalid read_on date"}, 400
-            
-            q.read_on = read_on
-
-        if 'content' in body:
-            q.content = body['content']
+        if read_on < q.sent_on or read_on > datetime.datetime.now():
+            return {"error": "Invalid read_on date"}, 400
+        
+        q.read_on = read_on
         
         db.session.commit()
+        return q.serialize()
 
 def create_app(db_path):
     logging.basicConfig(level=logging.INFO)
@@ -79,6 +103,7 @@ def create_app(db_path):
     # set the WSGI application callable to allow using uWSGI:
     # uwsgi --http :8080 -w app
     application = app.app
+    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     application.config['SQLALCHEMY_DATABASE_URI'] = db_path
     db.init_app(application)
     db.create_all(app=application)
